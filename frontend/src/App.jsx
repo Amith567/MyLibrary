4bd9b4b81dacb5a42389f92c4337798023436a65
Amith P <amithanilp@gmail.com>
2026-04-05 00:47:14 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (<>
+    <div className='flex items-center justify-center text-5xl w-full mt-10 text-red-700'>MyLibrary</div>
+    <p className='text-center mt-2'>this is a library management platform.</p>
+    </>
+  )
+}
+
+export default App
