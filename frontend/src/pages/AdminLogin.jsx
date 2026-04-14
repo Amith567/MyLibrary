481b0766a2f0561c63f42473bdd4f35969f92471
Amith P <amithanilp@gmail.com>
2026-04-14 17:33:31 +0530

    login page desighned

--- frontend/src/pages/AdminLogin.jsx
+++ frontend/src/pages/AdminLogin.jsx
@@ -3,15 +3,23 @@ import React from 'react'
 const AdminLogin = () => {
   return (
     <>
-<div className="flex h-screen">
-  <section className="w-1/2 bg-blue-700 text-white flex items-center justify-center">
-    hi
-  </section>
-  
-  <section className="w-1/2 bg-red-500 text-white flex items-center justify-center">
-    amith
-  </section>
-</div>
+      <div className="flex flex-col md:flex-row h-screen">
+        
+        <section className="w-full md:w-1/2   flex items-center justify-center">
+          <div className="p-5">
+            <img 
+              src="/login.jpg" 
+              alt="login" 
+              className="w-full max-w-md h-auto object-contain"
+            />
+          </div>
+        </section>
+
+        <section className="w-full md:w-1/2 bg-red-500 text-white flex items-center justify-center">
+          amith
+        </section>
+
+      </div>
     </>
   )
 }
