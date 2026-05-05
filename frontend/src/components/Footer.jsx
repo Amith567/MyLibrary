66a6aaf269c3f21e7c90b4350d01edf2b025c931
Amith P <amithanilp@gmail.com>
2026-05-05 20:18:59 +0530

    home page completed

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='w-full bg-blue-300 h-32'>Footer</div>
+  )
+}
+
+export default Footer
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className='flex flex-col min-h-screen'>
+<Navbar/>
+<div className='flex-1 flex justify-around items-center '>
+<div className='p-24 bg-blue-900 text-white hover:bg-blue-600 rounded-md text-sm font-bold'>Students</div>
+<div className='p-24 bg-blue-900 text-white hover:bg-blue-600 rounded-md text-sm font-bold'>Books</div>
+<div className='p-24 bg-blue-900 text-white hover:bg-blue-600 rounded-md text-sm font-bold'>Transactions</div>
+</div>
+<Footer/>
+
+    </div>
   )
 }
 
