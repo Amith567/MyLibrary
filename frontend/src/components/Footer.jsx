d402a100b3cd4b0cd0eec4d555b33164a7b22459
Amith P <amithanilp@gmail.com>
2026-05-07 01:46:52 +0530

    footer complete

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='w-full bg-blue-300 h-32'>Footer</div>
+    <div className='w-full bg-blue-900 text-white h-auto p-2 text-center'>© MyLibrary 2026</div>
   )
 }
 
