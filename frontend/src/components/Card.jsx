3a409832bd6340c4500713814e72c4466f4791b5
Amith P <amithanilp@gmail.com>
2026-05-20 21:01:06 +0530

    student list integrated

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({name,department,register_no}) => {
   return (
     <>
   <div className="bg-gray-800 rounded-lg p-3 flex items-center justify-between h-20 mb-4">
@@ -8,31 +8,31 @@ const Card = () => {
 
     <div className="flex items-center gap-4">
       <img
-        src="https://i.pravatar.cc/100"
+        src="/profile.jpg"
         alt="student"
         className="w-14 h-14 rounded-full object-cover"/>
       <div>
         <p className="text-lg font-semibold">
-          Amith P
+          {name}
         </p>
 
         <p className="text-sm text-gray-400">
-          LIB2026001
+          Branch: {department} | Reg no: {register_no}
         </p>
       </div>
 
     </div>
     <div className="flex gap-2">
 
-      <button className="bg-blue-500 px-3 py-1 rounded hover:bg-blue-600 transition">
+      <button className="bg-blue-600 px-3 py-1 rounded hover:bg-blue-500 transition">
         View
       </button>
 
-      <button className="bg-yellow-500 px-3 py-1 rounded hover:bg-yellow-600 transition">
+      <button className="bg-green-700 px-3 py-1 rounded hover:bg-green-600 transition">
         Edit
       </button>
 
-      <button className="bg-red-500 px-3 py-1 rounded hover:bg-red-600 transition">
+      <button className="bg-red-800 px-3 py-1 rounded hover:bg-red-600 transition">
         Delete
       </button>
 
--- frontend/src/pages/StudentsList.jsx
+++ frontend/src/pages/StudentsList.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Card from '../components/Card'
+import API from '../api/api'
 
 const StudentsList = () => {
+  const [students,setStudents]=useState([])
+  useEffect(()=>{
+    const fetchStudents=async()=>{
+      try{
+      const res=await API.get('/student/list/')
+    setStudents(res.data)
+  }catch(error){
+    alert(error.response?.data?.error || "something went wrong")
+  }
+    }
+    fetchStudents()
+  },[])
   return (
     <div className="bg-[#0F172A] min-h-screen p-5 text-gray-200">
 
   <h1 className="text-2xl font-bold mb-5">
     Students List
   </h1>
-<Card/><Card/><Card/><Card/>
+  {students.map((student)=>(<Card key={student.id} name={student.name} department={student.department} register_no={student.register_no} />))}
+
 </div>
   )
 }
