8d6b9a904aebba9543e8661391d7c308f228eb46
Amith P <amithanilp@gmail.com>
2026-07-06 10:32:50 +0530

    responsiveness added to book,student and transaction header section

--- frontend/src/BookPage.jsx
+++ frontend/src/BookPage.jsx
@@ -16,17 +16,17 @@ const BookPage = () => {
             }
         }
         loadbooks()
-
     },[])
   return (
     <div className='min-h-screen'>
         <div className='w-full flex justify-between flex-col md:flex-row gap-4'>
             <div className='text-xl md:text-2xl font-bold'>Books</div>
-            <form action="" className='flex gap-10'>
-                <input type="text" className='px-3 py-2 rounded-md border-1 border-gray-300 w-80 text-sm outline-none' placeholder='Search book...'/>
+            <div className='flex gap-2 flex-col md:flex-row'>
+            <form action="" className=''>
+                <input type="text" className='px-3 py-2 rounded-md border-1 border-gray-300 w-80 text-sm outline-none ' placeholder='Search book...'/>
             </form>
             <button className='w-40 h-10 bg-blue-500 flex justify-center gap-5 items-center text-white rounded-md'><FiPlus className='text-white text-2xl'/>Add Book</button>
-
+            </div>
         </div>
         <div className='w-full mt-5'>
             <div className='flex justify-evenly bg-gray-100 py-3 rounded-md'>
--- frontend/src/TransactionPage.jsx
+++ frontend/src/TransactionPage.jsx
@@ -7,11 +7,13 @@ const TransactionPage = () => {
     <div className='min-h-screen'>
         <div className='w-full flex justify-between flex-col md:flex-row gap-4'>
             <div className='text-xl md:text-2xl font-bold'>Transactions</div>
-            <form action="" className='flex gap-10'>
+            <div className="flex gap-2 flex-col lg:flex-row">
+            <form action="" className=''>
                 <input type="text" className='px-3 py-2 rounded-md border-1 border-gray-300 w-80 text-sm outline-none' placeholder='Search transaction...'/>
             </form>
             <button className='w-50 h-10 bg-blue-500 flex justify-center gap-5 items-center text-white rounded-md'><FiPlus className='text-white text-2xl'/>Create Transaction</button>
             <button className='w-50 h-10 bg-red-500 flex justify-center gap-5 items-center text-white rounded-md'><IoMdClose className='text-white text-2xl'/>Close Transaction</button>
+            </div>
         </div>
         <div className='w-full mt-5'>
             <div className='flex justify-evenly bg-gray-100 py-3 rounded-md'>
